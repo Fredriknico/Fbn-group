444196818d8b441f99dff4ed9696d509c9e63387
fredriknico <fredrik_nico@yahoo>
2020-05-20 13:54:19 +0200

    op

---
+++ routes/api/booking/index.js
@@ -0,0 +1,28 @@
+﻿const express = require('express');
+const api = express.Router();
+const selectAll = require('./all');
+const insertItem = require('./insert');
+const deleteItem = require('./delete');
+const singelItem = require('./item');
+const updateItem = require('./updateitem');
+const uploads = require('./uploads');
+
+// GET ALL ITEMS
+api.use('/bookings', selectAll);
+
+// INSERT ITEM
+api.use('/insert', insertItem);
+
+// DELETE ITEM
+api.use('/delete', deleteItem);
+
+// SELECT ITEM
+api.use('/item', singelItem);
+
+// UPDATE ITEM
+api.use('/update', updateItem);
+
+// IMAGES
+api.use('/uploads', uploads);
+
+module.exports = api;
