5c652760a6430283ab4a2dba8daf7e066e9f707c
fredriknico <fredrik_nico@yahoo>
2020-05-26 12:17:12 +0200

    Oppdatert

--- routes/api/index.js
+++ routes/api/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // API  MODULE
 router.use('/storageapp', require('./StorageApp'));
 
-// router.use('/booking', require('./booking'));
+router.use('/booking', require('./booking'));
 
 module.exports = router;
