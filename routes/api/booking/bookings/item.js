2caf04b130c3f0fbcfd60a8a6e2df2a8805af10d
fredriknico <fredrik_nico@yahoo>
2020-05-27 13:59:05 +0200

    Oppdatert

--- routes/api/booking/bookings/item.js
+++ routes/api/booking/bookings/item.js
@@ -2,11 +2,22 @@
 const db = require('../../../../database/nicobu');
 const router = express.Router();
 
-// Select storage item
+// Select booking by id and date
 router.get('/:id/:date', (req, res) => {
   let sql = `SELECT * FROM bookings WHERE eventid =${req.params.id} AND start =${req.params.date}`;
   db.query(sql, (err, result) => {
-    res.sendStatus(200);
+    res.send(result);
+    if (err) throw err;
+    res.sendStatus(501);
+    res.send(err);
+  });
+});
+
+// Select booking by id
+
+router.get('/:id', (req, res) => {
+  let sql = `SELECT * FROM bookings WHERE eventid =${req.params.id}`;
+  db.query(sql, (err, result) => {
     res.send(result);
     if (err) throw err;
     res.sendStatus(501);
