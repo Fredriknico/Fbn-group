d454c7082fa008ace0cf9a20b88332a5ec7d7701
fredriknico <fredrik_nico@yahoo>
2020-05-27 14:13:29 +0200

    Oppdatert

--- routes/api/booking/bookings/customer.js
+++ routes/api/booking/bookings/customer.js
@@ -7,7 +7,6 @@ router.get('/:id/:date', (req, res) => {
   let sql = `SELECT * FROM bookings WHERE eventid =${req.params.id} AND start =${req.params.date}`;
   db.query(sql, (err, result) => {
     res.send(result);
-
     if (err) throw err;
     res.sendStatus(501);
     res.send(err);
@@ -19,14 +18,7 @@ router.get('/:id/:date', (req, res) => {
 router.get('/:id', (req, res) => {
   let sql = `SELECT * FROM bookings WHERE eventid =${req.params.id}`;
   db.query(sql, (err, result) => {
-    if (res.statusCode == 404) {
-      res.send('ingenbooking');
-    }
-    if (res.length < 1) {
-      res.send('Ingen bookinger denne dagen');
-    } else {
-      res.send(result);
-    }
+    res.send(result);
     if (err) throw err;
     res.sendStatus(501);
     res.send(err);
